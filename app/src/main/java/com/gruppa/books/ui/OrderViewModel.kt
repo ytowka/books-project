6ffce074664ab8b3d151047ba33e9632f7da6171
grotor1 <varukhin04@mail.ru>
2023-07-09 13:06:21 +0300

    add navigation and binding to order details page

---
+++ app/src/main/java/com/gruppa/books/ui/OrderViewModel.kt
@@ -0,0 +1,102 @@
+package com.gruppa.books.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.gruppa.books.models.Book
+
+class OrderViewModel : ViewModel() {
+    private val testBooks: List<Book> = listOf(
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+        Book(
+            id = 0,
+            name = "Письма к брату Тео",
+            imageUrl = "https://cdn.ast.ru/v2/ASE000000000833774/COVER/cover1__w600.jpg",
+            author = "Ван Гог Винсент",
+            editionYear = 2004,
+            description = "",
+            mark = 5f,
+            price = 240,
+            inCartCount = 15,
+        ),
+    )
+
+    private val _books = MutableLiveData(testBooks)
+    val books: LiveData<List<Book>> = _books
+}
